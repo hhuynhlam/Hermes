e6ee12738935f963fd456e5c2f53bc4ef0bb1162
Hai Huynhlam <hhuynhla@advent.com>
2015-11-12 18:33:08 -0800

    login validation

--- src/client/screens/login/login.viewmodel.js
+++ src/client/screens/login/login.viewmodel.js
@@ -1,9 +1,40 @@
 'use strict';
 
+import ko from 'knockout';
+
 class LoginViewModel {
-    constructor() {}   
+    constructor(options) {
+        this.options = options || {};
+
+        this.email = ko.observable('');
+        this.emailError = ko.observable(false);
+
+        this.password = ko.observable('');
+        this.pwError = ko.observable(false);
+    }   
 
     init() {}
+
+    login() {
+        this._validate();
+    }
+
+    _validate() {
+        if (!this.email() || !this.password()) {
+            if (!this.email()) { this.emailError(true); }
+            else { this.emailError(false); }
+
+            if (!this.password()) { this.pwError(true); }
+            else { this.pwError(false); }
+
+            return false;
+        
+        } else {
+            this.emailError(false);
+            this.pwError(false);
+            return true;
+        }
+    }
 }
 
 export default LoginViewModel;
