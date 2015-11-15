35b6f2b2bb68d9a509eb8685ef436e70614439ff
Hai Huynhlam <hhuynhla@advent.com>
2015-11-15 00:41:45 -0800

    db-connector

--- src/server/helpers/db-connector.js
+++ src/server/helpers/db-connector.js
@@ -1,26 +1,24 @@
 'use strict';
 
 var pg = require('pg');
+
 var connectionString = "postgres://db_admin:haiwashere99@localhost/family";
+var client = new pg.Client(connectionString);
 
-var client = new pg.Client(connectionString),
-    results;
+var db = {
+        query: function (queryString, res) {
+            client.connect(function(err) {
+                if (err) { return res.status(500).send('could not connect to postgres', err); }
 
-var db {
-        query = function (queryString, callback) {
+                client.query(queryString, function(err, result) {
+                    if (err) { return res.status(500).send('error running query', err); }
+                    
+                    client.end();
+                    return result;
+                });
+            }); 
 
-        client.connect(function(err) {
-            if (err) { return res.status(500).send('could not connect to postgres', err); }
-            
-            client.query(queryString, function(err, result) {
-                if (err) { return res.status(500).send('error running query', err); }
-                
-                client.end();
-                callback.call(result);
-            });
-        }); 
-
-    }
+        }
 };
 
 module.exports = db;
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var db = require('../helpers/db-connector');
-var qb = require('../helpers/query-builder');
+var queryBuilder = require('../helpers/query-builder');
 var express = require('express');
 var router = express.Router();
 
@@ -13,10 +13,10 @@ var router = express.Router();
 // router.get('/', passport.authenticate('local', { session: false }),
 router.get('/',
 function (req, res) {
-    var queryString = qb(req.body);
-    db.query(queryString, function(res) {
-        console.log(res);
-    });
+    var queryString = queryBuilder(req.body);
+
+    var data = db.query('select * from Users', res);
+    return res.status(200).json( (data && data.length) ? data[0] : [] );
 });
 
 
