5e7483459096b17ad30046d9a0be7e5a81891c61
Hai Huynhlam <hhuynhla@advent.com>
2015-11-14 22:07:23 -0800

    postgres test

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,24 +1,10 @@
 'use strict';
 
-var _ = require('lodash');
 var express = require('express');
-var jsonfile = require('jsonfile');
-// var passport = require('passport');
+var pg = require('pg');
 var router = express.Router();
 
-var Users;
-
-
-//======================================
-// Mock
-//======================================
-
-var userFile = 'src/server/_mock/Users.json';
-jsonfile.spaces = 4;
-jsonfile.readFile(userFile, function(err, data) {
-    if (err) { console.error(err); }
-    else { Users = data; }
-});
+var connectionString = "postgres://username:password@localhost/database";
 
 
 //======================================
@@ -29,78 +15,24 @@ jsonfile.readFile(userFile, function(err, data) {
 // router.get('/', passport.authenticate('local', { session: false }),
 router.get('/',
 function (req, res) {
-    return res.status(200).json(Users);
-});
+    var client = new pg.Client(connectionString),
+        results;
 
-// Create a User
-router.put('/',
-function (req, res) {
-    var prevUser = _.last(Users),
-        index = (prevUser) ? parseInt(prevUser.id) + 1 : 1,
-        user;
-    
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).send('Error: Missing email or password.');
-    
-    } else if ( _.find(Users, function (u) { return u.email === req.body.email ; })) {
-        return res.status(400).send('Error: Email already in use.');
-    
-    } else {
-        user = { id: index.toString() };
-        user = _.assign(user, req.body);
-        Users.push(user);
+    client.connect(function(err) {
+        if (err) { return res.status(500).send('could not connect to postgres', err); }
+        
+        client.query('SELECT NOW() AS "theTime"', function(err, result) {
+            
+            if (err) { return res.status(500).send('error running query', err); }
+            
+            results = result.rows[0];
+            client.end();
 
-        jsonfile.writeFile(userFile, Users, function (err) {
-            if (err) { return res.status(500).send('Error: ' + err); }
-            else { return res.status(201).json(user); } 
         });
-    }
-});
 
-// Get a User by id
-router.get('/:id',
-function (req, res) {
-    var user = _.find(Users, function (u) {
-        return u.id === req.params.id;
+        return res.status(200).json(results);
     });
-
-    return (user) ? res.status(200).json(user) : res.status(404).send('No User Found.');
 });
 
-// Update a User by id
-router.put('/:id',
-function (req, res) {
-    var userIndex = _.findIndex(Users, function (u) { 
-        return u.id === req.params.id; 
-    });
-    
-    if (userIndex < 0) {
-        return res.status(404).send('No User Found.');
-    } else {
-        Users[userIndex] = _.assign(Users[userIndex], req.body);
-        jsonfile.writeFile(userFile, Users, function (err) {
-            if (err) { return res.status(500).send('Error: ' + err); }
-            else { return res.status(201).json(Users[userIndex]); } 
-        });
-    }
-});
-
-// Delete a User by id
-router.delete('/:id',
-function (req, res) {
-    var user = _.find(Users, function (u) {
-        return u.id === req.params.id;
-    });
-    
-    if (!user) {
-        return res.status(404).send('No User Found.');
-    } else {
-         _.remove(Users, user);
-        jsonfile.writeFile(userFile, Users, function (err) {
-            if (err) { return res.status(500).send('Error: ' + err); }
-            else { return res.status(200).send('User Delete Success.'); } 
-        });
-    }
-});
 
 module.exports = router;
---
+++ src/server/routes/users_mock.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var _ = require('lodash');
+var express = require('express');
+var jsonfile = require('jsonfile');
+// var passport = require('passport');
+var router = express.Router();
+
+var Users;
+
+
+//======================================
+// Mock
+//======================================
+
+var userFile = 'src/server/_mock/Users.json';
+jsonfile.spaces = 4;
+jsonfile.readFile(userFile, function(err, data) {
+    if (err) { console.error(err); }
+    else { Users = data; }
+});
+
+
+//======================================
+// Routes
+//======================================
+
+// List all Users
+// router.get('/', passport.authenticate('local', { session: false }),
+router.get('/',
+function (req, res) {
+    return res.status(200).json(Users);
+});
+
+// Create a User
+router.put('/',
+function (req, res) {
+    var prevUser = _.last(Users),
+        index = (prevUser) ? parseInt(prevUser.id) + 1 : 1,
+        user;
+    
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send('Error: Missing email or password.');
+    
+    } else if ( _.find(Users, function (u) { return u.email === req.body.email ; })) {
+        return res.status(400).send('Error: Email already in use.');
+    
+    } else {
+        user = { id: index.toString() };
+        user = _.assign(user, req.body);
+        Users.push(user);
+
+        jsonfile.writeFile(userFile, Users, function (err) {
+            if (err) { return res.status(500).send('Error: ' + err); }
+            else { return res.status(201).json(user); } 
+        });
+    }
+});
+
+// Get a User by id
+router.get('/:id',
+function (req, res) {
+    var user = _.find(Users, function (u) {
+        return u.id === req.params.id;
+    });
+
+    return (user) ? res.status(200).json(user) : res.status(404).send('No User Found.');
+});
+
+// Update a User by id
+router.put('/:id',
+function (req, res) {
+    var userIndex = _.findIndex(Users, function (u) { 
+        return u.id === req.params.id; 
+    });
+    
+    if (userIndex < 0) {
+        return res.status(404).send('No User Found.');
+    } else {
+        Users[userIndex] = _.assign(Users[userIndex], req.body);
+        jsonfile.writeFile(userFile, Users, function (err) {
+            if (err) { return res.status(500).send('Error: ' + err); }
+            else { return res.status(201).json(Users[userIndex]); } 
+        });
+    }
+});
+
+// Delete a User by id
+router.delete('/:id',
+function (req, res) {
+    var user = _.find(Users, function (u) {
+        return u.id === req.params.id;
+    });
+    
+    if (!user) {
+        return res.status(404).send('No User Found.');
+    } else {
+         _.remove(Users, user);
+        jsonfile.writeFile(userFile, Users, function (err) {
+            if (err) { return res.status(500).send('Error: ' + err); }
+            else { return res.status(200).send('User Delete Success.'); } 
+        });
+    }
+});
+
+module.exports = router;
