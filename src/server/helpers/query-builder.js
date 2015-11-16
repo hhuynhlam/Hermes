7b787404f9694294dcf8db4bf20024dcf066b6cb
Steve <skly@ps486964.dreamhostps.com>
2015-11-15 19:12:55 -0800

    simple query builder.  have to think more about types

--- src/server/helpers/query-builder.js
+++ src/server/helpers/query-builder.js
@@ -1,7 +1,44 @@
 'use strict';
 
 var queryBuilder = function(body) {
-    return 'Hello World';
+    var query, columns, table, where, limit;
+
+    // Build columns of query
+    if (body.select instanceof Array) {
+        columns = body.select.join(',');
+    }
+    else {
+        columns = "*";
+    }
+    query = "SELECT " + columns;
+
+    // determine table name 
+    if (body.route) {
+        table = body.route; 
+    }
+    else {
+        return "Route not defined";
+    }
+    query += " FROM " + table;
+    
+    // Build where clause
+    if (body.where instanceof Array) {
+        body.where.forEach(function(eachWhere) {
+            where = eachWhere.key + eachWhere.operator + "'" + eachWhere.value + "'";   
+        });
+        query += " WHERE " + where;
+    }
+
+    // Build offset, limit clause
+    if (body.offset) {
+        query += " OFFSET " + body.offset;
+    }
+
+    if (body.limit) {
+        query += " LIMIT " + body.limit;
+    }
+
+    return query;
 };
 
 module.exports = queryBuilder;
--- src/server/helpers/sly-test.js
+++ src/server/helpers/sly-test.js
@@ -1,5 +1,15 @@
 var qb = require('./query-builder');
 
-var test = qb({});
+var test = qb({
+                'select' : [ 'email' ],
+                'route'  : 'Users',
+                'where'  : [ 
+                             { 'key'   : 'lastname',
+                               'value' : 'Huynh',
+                               'operator' : '=',
+                             }
+                           ],
+                'offset' : '3',
+              });
 
 console.log(test);
