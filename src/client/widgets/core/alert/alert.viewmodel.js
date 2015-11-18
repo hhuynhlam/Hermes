72204321c5544ded69cc7dbcb831fbf0a67e1efa
Hai Huynhlam <hhuynhla@advent.com>
2015-11-17 18:45:52 -0800

    cleanup alerts

--- src/client/widgets/core/alert/alert.viewmodel.js
+++ src/client/widgets/core/alert/alert.viewmodel.js
@@ -5,85 +5,48 @@ import BaseWidgetViewModel from 'base-widget.viewmodel';
 import 'k/kendo.notification.min';
 
 var msg = sandbox.msg;
+var _ = sandbox.util;
 
 class AlertViewModel extends BaseWidgetViewModel {
-  constructor(options) {
-    super(options);
+    constructor(options) {
+        super(options);
 
-    this.options = options || {};
-  }
-  
-  init() {
-    this.setOptions();
-    this.$selector.kendoNotification(this.options);
-    this.widget = this.$selector.data("kendoNotification");
-  }
-  
-  setOptions() {
-    var _supportedEvents = ['hide', 'show'];
-    this.setupPublications(_supportedEvents);
-    this.setupSubscriptions();
-  }
-
-  setupSubscriptions() {
-    var _subscription, _topic;
-
-    // dispose any existing subscriptions
-    msg.dispose.apply(this, this.subscriptions);
+        this.options = options || {};
+    }
     
-    // error
-    if (this.options.subscribe && this.options.subscribe.error) {
-        _topic = this.options.subscribe.error;
-
-        // create new subscriptions
-        _subscription = msg.subscribe(_topic, (val) => {
-            this.widget.error(val);
-        }, this, true);
-
-        // track subscriptions
-        this.subscriptions.push(_subscription);
+    init() {
+        this.setOptions();
+        this.$selector.kendoNotification(this.options);
+    }
+    
+    setOptions() {
+        var _supportedEvents = ['hide', 'show'];
+        this.setupPublications(_supportedEvents);
+        this.setupSubscriptions();
     }
 
-    // success
-    if (this.options.subscribe && this.options.subscribe.success) {
-        _topic = this.options.subscribe.success;
+    setupSubscriptions() {
+        var _subscription, _topic;
 
-        // create new subscriptions
-        _subscription = msg.subscribe(_topic, (val) => {
-            this.widget.success(val);
-        }, this, true);
+        // dispose any existing subscriptions
+        msg.dispose.apply(this, this.subscriptions);
+        
+        // supported subscriptions: error, success, info, warning
+        if (this.options.subscribe) {
+            _.forOwn(this.options.subscribe, (val, key) => {
+                _topic = val;
 
-        // track subscriptions
-        this.subscriptions.push(_subscription);
+                // create new subscriptions
+                _subscription = msg.subscribe(_topic, (val) => {
+                    this.$selector.data("kendoNotification")[key](val);
+                }, this, true);
+
+                // track subscriptions
+                this.subscriptions.push(_subscription);
+            });
+
+        }
     }
-
-    // info
-    if (this.options.subscribe && this.options.subscribe.info) {
-        _topic = this.options.subscribe.info;
-
-        // create new subscriptions
-        _subscription = msg.subscribe(_topic, (val) => {
-            this.widget.info(val);
-        }, this, true);
-
-        // track subscriptions
-        this.subscriptions.push(_subscription);
-    }
-
-    // warning
-    if (this.options.subscribe && this.options.subscribe.warning) {
-        _topic = this.options.subscribe.warning;
-
-        // create new subscriptions
-        _subscription = msg.subscribe(_topic, (val) => {
-            this.widget.warning(val);
-        }, this, true);
-
-        // track subscriptions
-        this.subscriptions.push(_subscription);
-    }
-
-  }
 }
 
 export default AlertViewModel;
