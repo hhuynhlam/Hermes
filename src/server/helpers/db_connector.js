739b1fd579843d5ee5cd859ad845666c64e77b6d
Hai Huynhlam <hhuynhla@advent.com>
2015-11-22 11:25:03 -0800

    switch to MySQL and bump node version

--- src/server/helpers/db_connector.js
+++ src/server/helpers/db_connector.js
@@ -1,38 +1,18 @@
 'use strict';
 
 var path = require('path');
+var mysql = require('mysql');
 var _config = require(path.resolve(__dirname, '../prod.config.json'));
-var pg = require('pg');
-
-var connectionString = _config.connectionString;
+var connection = mysql.createConnection(_config.db);
 
 var db = {
     query: function (queryString, success, fail) {
-        pg.connect(connectionString, function(err, client, done) {
-            var results = [],
-                query;
-
-            // Handle connection errors
-            if(err) {
-                done();
-                fail.call(this, err);
-            }
-
-            // SQL Query
-            query = client.query(queryString);
-
-            // // Stream results back one row at a time
-            query.on('row', function(row) {
-                results.push(row);
-            });
-
-            // After all data is returned, close connection and return results
-            query.on('end', function() {
-                done();
-                success.call(this, results);
-            });
-
-
+        connection.connect();
+        connection.query(queryString, function(err, rows, fields) {
+            debugger;
+            if (err) { fail.call(this, err); }
+            else { success.call(this, rows); }
+            connection.end();
         });
     }
 };
