dbc40176e6e13081de3ee700ca2b7b5f471373c5
Hai Huynhlam <hhuynhla@advent.com>
2015-11-25 19:20:30 -0800

    jshint

--- src/server/routes/photos.js
+++ src/server/routes/photos.js
@@ -16,7 +16,6 @@ var router = express.Router();
 router.post('/',
 function (req, res) {
     var queryString = queryBuilder.select({ route: 'vPhoto', where: [{'key':'pgid', 'operator':'=', 'value':'62' }] });
-    debugger;
     db.query(queryString, function (data) {
         return res.status(200).json( data );
     }, function (err) { res.status(500).send('SQL Error: ' + err); });
