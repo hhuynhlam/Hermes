5548adf484cf21439b7af57b7cbaba9bfa9918b2
Hai Huynhlam <hhuynhla@advent.com>
2015-11-25 19:26:33 -0800

    sum link images

--- build/dev/remote.sh
+++ build/dev/remote.sh
@@ -13,6 +13,9 @@ mv _dist hermes
 ln -s /home/webservice/_config/prod.config.json ./hermes/src/server/prod.config.json 
 chmod 755 ./hermes/src/server/prod.config.json 
 
+ln -s /home/webservice/images ./hermes/src/server/public/uploaded/images
+chmod 755 ./hermes/src/server/public/uploaded/images 
+
 cd hermes && npm install --production
 
 /usr/local/bin/svc -u /service/hermes 
