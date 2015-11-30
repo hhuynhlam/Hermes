818df09a132d3c98fe9e03b208d103322f51f53b
Hai Huynhlam <hhuynhla@advent.com>
2015-11-30 06:10:04 -0800

    just formatting

--- db/tableLayouts.sql
+++ db/tableLayouts.sql
@@ -18,16 +18,16 @@ CREATE TABLE Users
 
 
 CREATE TABLE `Photos` (
-  `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `photoName` varchar(255) DEFAULT NULL,
-  `photoExt` varchar(12) DEFAULT NULL,
-  `pgid` int(11) DEFAULT NULL,
-  `uid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`pid`)
+    `pid` int(11) NOT NULL AUTO_INCREMENT,
+    `photoName` varchar(255) DEFAULT NULL,
+    `photoExt` varchar(12) DEFAULT NULL,
+    `pgid` int(11) DEFAULT NULL,
+    `uid` int(11) DEFAULT NULL,
+    PRIMARY KEY (`pid`)
 );
 
 CREATE TABLE `PhotosGroup` (
-  `pgid` int(11) NOT NULL AUTO_INCREMENT,
-  `groupName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`pgid`)
+    `pgid` int(11) NOT NULL AUTO_INCREMENT,
+    `groupName` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`pgid`)
 );
