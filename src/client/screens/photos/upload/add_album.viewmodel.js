73526dd2d502bee893ae2eec356a45ca7083d163
Hai Huynhlam <hhuynhla@advent.com>
2015-12-11 07:10:25 -0800

    [Add Album] progress bars and multi-step setup

--- src/client/screens/photos/upload/add_album.viewmodel.js
+++ src/client/screens/photos/upload/add_album.viewmodel.js
@@ -1,28 +1,77 @@
 'use strict';
 
 import $ from 'jquery';
-// import ko from 'knockout';
+import ko from 'knockout';
 import sandbox from 'sandbox';
+import buttonWidget from 'button.widget';
 
 var msg = sandbox.msg;
 
 class AddAlbumViewModel {
     constructor(options) {
         this.options = options || {};
+        this.step = ko.observable(1);
     }   
 
-    init(albumId) {
-        this.albumId = albumId;        
+    init() {      
         this._createWidgets();
         this._setupEvents();
     }
 
     _createWidgets() {
-      
+
+        // ----------------------------------------
+        // Buttons
+        
+        buttonWidget.create({
+            id: 'AddAlbumPrev',
+            label: 'Back',
+            styles: [
+                'btn-default',
+                'btn-block'
+            ],
+            trigger: {
+                click: ['AddAlbumPrev.Click']
+            }
+        });
+
+        buttonWidget.create({
+            id: 'AddAlbumNext',
+            label: 'Next',
+            styles: [
+                'btn-primary',
+                'btn-block',
+            ],
+            trigger: {
+                click: ['AddAlbumNext.Click']
+            }
+        });
+
+        buttonWidget.create({
+            id: 'AddAlbumDone',
+            label: 'Done',
+            styles: [
+                'btn-primary',
+                'btn-block',
+            ],
+            trigger: {
+                click: ['AddAlbumDone.Click']
+            }
+        });
+
+        // ----------------------------------------
     }
 
     _setupEvents() {
-        
+        var $eventElement = $(document);
+
+        $eventElement.on('AddAlbumPrev.Click', () => {
+            this.step( this.step() - 1  );
+        });
+
+        $eventElement.on('AddAlbumNext.Click', () => {
+            this.step( this.step() + 1  );
+        });
     }
 }
 
