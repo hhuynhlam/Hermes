702ed29d17914045088b0ef49802cef8140a5b06
steve <steve.ly@gmail.com>
2015-12-13 21:02:08 +0000

    Create Notifications

---
+++ db/tables/Notifications.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Notifications` (
+  `nid` int(11) NOT NULL AUTO_INCREMENT,
+  `nType` varchar(12) DEFAULT NULL,
+  `nTarget` int(11) DEFAULT NULL,
+  `nUpdateDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`nid`)
+)
