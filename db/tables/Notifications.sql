8ba5d5a96f28dbc597af757ecccd1ed5f061d5d5
steve <steve.ly@gmail.com>
2015-12-16 07:36:08 +0000

    Added uid to Notifications table

--- db/tables/Notifications.sql
+++ db/tables/Notifications.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `Notifications` (
   `nid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11)  DEFAULT NULL,
   `nType` varchar(12) DEFAULT NULL,
   `nTarget` int(11) DEFAULT NULL,
   `nUpdateDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
