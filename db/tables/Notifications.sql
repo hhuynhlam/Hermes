ed51f04e6cd71e2ca3a9833c31d78d8d801f35c8
steve <steve.ly@gmail.com>
2015-12-21 06:05:43 +0000

    fixed order of notify

--- db/tables/Notifications.sql
+++ db/tables/Notifications.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `Notifications` (
   `nid` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11)  DEFAULT NULL,
   `nType` varchar(12) DEFAULT NULL,
   `nTarget` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
   `nUpdateDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`nid`)
 )
