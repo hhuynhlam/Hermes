17b253206ece852ef6693fe5bcc6a30e2d6921dd
steve <steve.ly@gmail.com>
2015-12-21 06:07:02 +0000

    fixed order of notify

--- db/procedures/Notifications/pCreateNotification.sql
+++ db/procedures/Notifications/pCreateNotification.sql
@@ -3,21 +3,21 @@ DROP PROCEDURE IF EXISTS `pCreateNotification`;
 DELIMITER //
 CREATE PROCEDURE `pCreateNotification` (
     IN _nType   varchar(12),
-    IN _uid int(11),
-    IN _nTarget int(11)
+    IN _nTarget int(11),
+    IN _uid int(11)
 )
 BEGIN
     
     INSERT INTO Notifications VALUES ( 
         NULL,
         _nType,
-        _uid,
         _nTarget,
+        _uid,
         NOW()
     );
 
     -- return new photos comment 
-    SELECT nid,nType,uid,nTarget,nUpdateDate FROM Notifications
+    SELECT nid,nType,nTarget,uid,nUpdateDate FROM Notifications
         ORDER BY nid DESC LIMIT 1;
         
 
