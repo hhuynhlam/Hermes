5b0a02ee176d326dd4a6e9e226e54ea0deb582e4
Hai Huynhlam <hhuynhlam@gmail.com>
2018-10-17 09:20:09 -0700

    feat(photo): add caption, height, width fields

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class PhotoSerializer < ApplicationSerializer
+  belongs_to :owner
+  has_many :tags
+
+  attributes :caption
+  attributes :height
+  attributes :width
   attributes :url
 
   def url
