af707f07a56c82dee7f64488be0e30706dfc4cc4
Hai Huynhlam <hhuynhlam@gmail.com>
2018-10-20 12:30:07 -0700

    feat(profile): open error/success snackbar on profile update

--- app/javascript/src/profile/sagas/index.js
+++ app/javascript/src/profile/sagas/index.js
@@ -2,6 +2,7 @@ import { all, call, put, takeEvery } from 'redux-saga/effects'
 import { invoke } from 'lodash/fp'
 import axios from 'axios'
 import immutableToJS from '../../common/services/immutableToJS'
+import { openSnackbar } from '../../snackbar'
 import * as actions from '../actions'
 
 function* updateProfileSaga({ payload }) {
@@ -22,11 +23,21 @@ function* updateProfileSaga({ payload }) {
       type: actions.UPDATE_PROFILE_SUCCESS,
       payload: data,
     })
+    yield put(openSnackbar({
+      message: 'Profile Updated!',
+      variant: 'success',
+    }))
   } catch (error) {
+    const errorMessage = invoke('toString')(error) || error
+
     yield put({
       type: actions.UPDATE_PROFILE_FAILED,
-      payload: invoke('toString')(error) || error,
+      payload: errorMessage,
     })
+    yield put(openSnackbar({
+      message: errorMessage,
+      variant: 'error',
+    }))
   }
 }
 
